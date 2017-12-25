086f11c2fdaae0b7801e3090405ab84ac00d0d8b
Bhavesh Gawri <4bhaveshgawri@gmail.com>
2017-12-26 03:29:00 +0530

    Update #define.cpp

--- #define.cpp
+++ #define.cpp
@@ -1,7 +1,11 @@
 #include <bits/stdc++.h>
 
-#define ll      long long int
+#define gc         getchar
+#define pc         putchar
+// #define gc         getchar_unlocked
+// #define pc         putchar_unlocked
 
+#define ll         long long
 #define nl         "\n"
 #define pb         push_back
 #define ppb        pop_back
@@ -20,17 +24,44 @@
 #define mne(a)        *min_element(all(a))
 
 #define lb(x, k)   lower_bound(all(x), k)-x.begin()
-//first element not less than (i.e. greater or equal to) k
+//first element greater or equal to k
 #define ub(x, k)   upper_bound(all(x), k)-x.begin()
 //first element greater than k
+
+// fast i/o for integers
+#define in2(a, b)        in(a), in(b)
+#define in3(a, b, c)     in2(a, b), in(c)
+#define in4(a, b, c, d)  in3(a, b, c), in(d)
+#define out2(a, b)       out(a), out(b)
+#define out3(a, b, c)    out2(a, b), out(c)
+#define out4(a, b, c, d) out3(a, b, c), out(d)
+
+template <typename T> bool in(T &n){
+    n = 0;
+    bool got = false, negative = false;
+    char c = gc();
+    while( c < '0' || c > '9'){if (c == '-') negative = true; c = gc();}
+    while(c >= '0' && c <= '9'){got = true; n = n*10 + c-48; c = gc();}
+    if(negative) n = ~(n-1);
+    return got;
+}
+template <typename T> void out(T n, char seperator = ' '){
+    if(n<0){pc('-'); n = -n;}
+    if(!n) {pc('0'); pc(seperator); return;}
+    char buff[22]; 
+    int len = 0;
+    while(n) buff[len++] = n%10+48,n/=10;
+    for(int i=len-1;i>=0;i--) pc(buff[i]);
+    pc(seperator);
+}
 
 #define inf      INT_MAX
 #define INF      LLONG_MAX
 
-#define ff    first
-#define ss    second
+#define ff       first
+#define ss       second
 
-using namespace std;
+using namespace  std;
 
 typedef pair<int, int> ii;
 typedef pair<ll, ll> II;
@@ -41,19 +72,18 @@ typedef vector<vl> vvl;
 typedef vector<ii> vii;
 typedef vector<vii> vvii;
 
-const int Max1 = 1e5 + 4;
-const int Max2 = 2e5 + 4;
-const int Mod = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 4;
 
 void solve(){
-	
+    
 }
 
 int main(){
-	nfs;
-	no_step;
-	//freopen("input.txt", "r", stdin);
-	//freopen("output.txt", "w", stdout);
-	solve();
-	return 0;
+    nfs;
+    no_step;
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
 }
